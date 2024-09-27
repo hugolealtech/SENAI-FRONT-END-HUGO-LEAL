e13e085052d92c7c510b63d52c1f9d2886b1bb1e
hugolealtech <76668742+hugolealtech@users.noreply.github.com>
2024-09-27 11:00:48 -0300

    Página de cadastro clientes

---
+++ JavaScript/Aula dia 27-09-2024 POO/script/construtor.js
@@ -0,0 +1,61 @@
+var clientes = []  //passo 01
+
+class Cliente {
+
+    constructor(nome, cpf, nConta,email,credito){ // Passo 02 - tirou o saldo do constructor e atribuiu 0 para conta nova
+
+        this.nome = nome;
+        this.cpf = cpf;
+        this.nConta = nConta;
+        this.email = email;
+        this.saldo = 0;
+        this.credito = credito;
+
+        this.fazerCompra = function(valor){}//passo 3
+
+        this.transferir = function(valor,cliente){}
+
+        this.receber = function(valor,cliente){}
+    }
+
+    receber = function(valor,cliente){}// posso criar métodos fora do constructor
+}
+
+function mostrarClientes(){
+
+    document.querySelector("#lista").innerHTML = ``//limpa lista
+    
+    clientes.forEach(cadaCliente => {
+
+        document.querySelector("#lista").innerHTML += `
+
+            <div class="cliente">
+
+                <p>${cadaCliente.nome}</p>   
+                <p>${cadaCliente.cpf}</p>         
+                <p>${cadaCliente.email}</p>
+            
+            </div>      
+        
+        `
+        
+    });
+
+
+}
+
+function cadastrarCliente(){
+
+    let nome = document.querySelector("#nome").value;
+    let cpf = document.querySelector("#cpf").value;
+    let nConta = document.querySelector("#nConta").value;
+    let email = document.querySelector("#email").value;
+    let credito = document.querySelector("#credito").value;
+
+
+    clientes.push (new Cliente (nome,cpf,nConta,email,credito));
+
+mostrarClientes();
+}
+
+document.querySelector("#cadastrar").addEventListener('click',cadastrarCliente)
